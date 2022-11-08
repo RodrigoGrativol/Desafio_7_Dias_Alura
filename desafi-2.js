d8421ab5c5b0268d46e02b3bb8f7a4e3fc16a2f4
Rodrigo Grativol <rodrigo_grativol@hotmail.com>
2022-11-08 17:39:20 -0300

    Desafio-2

---
+++ desafi-2.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos ans voce tem?");
+const linguagem = prompt("Qual a linguagem voce esta estudando?");
+const msg = `"Ola ${nome}, voce tem ${idade} anos e ja esta aprendendo ${linguagem}!"`;
+
+alert(msg);
