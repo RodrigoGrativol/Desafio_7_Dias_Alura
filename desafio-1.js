a8d0366d2938cedc643f0fef89b1816b8e42553b
Rodrigo Grativol <73142000+RodrigoGrativol@users.noreply.github.com>
2022-11-08 16:51:34 -0300

    Add files via upload

---
+++ desafio-1.js
@@ -0,0 +1,27 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm) {
+console.log('As variaveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+console.log('As variaveis numeroUm e stringUm nao tem o mesmo valor')
+}
+As variaveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes
+
+if (numeroTrinta === stringTrinta) {
+console.log('As variaveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+} else {
+console.log('As variaveis numeroTrinta e stringTrinta nao tem o mesmo valor')
+}
+As variaveis numeroTrinta e stringTrinta nao tem o mesmo valor
+
+if (numeroDez == stringDez) {
+console.log('As variaveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+console.log('As variaveis numeroDez e stringDez nao tem o mesmo valor')
+}
+As variaveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes
